c57684a019ba4d0973562135a87cffe0617ce90d
abumilhan78 <abumilhan78@gmail.com>
2019-08-19 09:05:37 +0700

    New Update!

---
+++ next-array.php
@@ -0,0 +1,13 @@
+<?php
+//Numeric Array
+$a = array('Milhan',21,true,14.76);
+
+for ($i=4; $i >= 0 ; $i--) { 
+    echo $a[$i] . "<br>";
+}
+
+//Associative Key Array
+$asso =['name'=>'Milhan','age'=>16,'adress'=>'Bandung'];
+foreach ($asso as $key => $value) {
+    echo $key . " : " . $value;
+}
