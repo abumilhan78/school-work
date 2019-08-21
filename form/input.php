61e0f5d46e863ada59d5ba6184f56d7677ef5b78
abumilhan78 <abumilhan78@gmail.com>
2019-08-21 11:32:57 +0700

    gabut...

---
+++ form/input.php
@@ -0,0 +1,9 @@
+<html>
+    <head><title>Pengolahan Form</title></head>
+    <body>
+        <FORM ACTION="proses02.php" METHOD="POST" NAME="input">
+            Nama Anda : <input type="text" name="nama"><br>
+            <input type="submit" name="Input" value="Input">
+        </FORM>
+    </body>
+</html>
