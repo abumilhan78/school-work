33d2616a3c801da18e6e118d61cebbb5abb884f2
abumilhan78 <abumilhan78@gmail.com>
2019-08-21 07:52:14 +0700

    lovely exam pt.1

---
+++ exam/case-1.php
@@ -0,0 +1,12 @@
+<?php
+$x = 100;
+$y = 50;
+
+echo "Condition : ($x==$y) || ($x>$y)";
+echo "<hr>Output : ";
+
+if (($x==$y) || ($x>$y)) {
+    echo "Привет мир(Hello World)";
+}else {
+    echo "Неверный ввод(Wrong Condition)";
+}
