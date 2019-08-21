d85739404ae20bbdb542c265053ee7a8f9eb443a
abumilhan78 <abumilhan78@gmail.com>
2019-08-21 08:32:45 +0700

    lovely exam pt.2.3

--- exam/case-3.php
+++ exam/case-3.php
@@ -3,7 +3,14 @@ for ($i=1; $i <= 10; $i++) {
     echo "Hitung Perkalian $i<br>";
     for ($j=1; $j <= 10; $j++) { 
         $f = $i*$j;
-        echo "$i * $j = $f<br>";
+        
+        echo "$i * $j = $f =>";
+        if ($f%2 == 1) {
+            echo "Bilangan Ganjil";
+        }else {
+            echo "Bilangan Genap";
+        }
+        echo "<br>";
     }
     echo "<br>";
 }
