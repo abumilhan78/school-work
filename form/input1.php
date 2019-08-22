e314c150652b0d46588db975bbcc7f3e02185e80
abumilhan78 <abumilhan78@gmail.com>
2019-08-22 07:56:29 +0700

    Student Work......

---
+++ form/input1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="output1.php" method="post">
+        Sahabat-sahabat Dekatku<br>
+        <input type="text" name="nama1"><br>
+        <input type="text" name="nama2"><br>
+        <input type="text" name="nama3"><br>
+        <input type="text" name="nama4"><br>
+        <input type="submit" name="Input" value="Input">
+    </form>
+</body>
+</html>
---
+++ form/output1.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['Input'])) {
+    $nama1 = $_POST['nama1'];
+    $nama2 = $_POST['nama2'];
+    $nama3 = $_POST['nama3'];
+    $nama4 = $_POST['nama4'];
+    echo "<b>Nama Sahabat-sahabat Dekatku :</b> <br>";
+    echo $nama1. "<br>";
+    echo $nama2. "<br>";
+    echo $nama3. "<br>";
+    echo $nama4. "<br>";
+    }
