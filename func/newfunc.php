fcab9571f88b61ad891decb263b68dc568004be4
abumilhan78 <abumilhan78@gmail.com>
2019-08-30 13:45:48 +0700

    something new!

--- func/newfunc.php
+++ func/newfunc.php
@@ -6,4 +6,15 @@ return 3.14 * $jari * $jari;
 $r = 10;
 echo "Luas lingkaran dengan jari-jari $r = ";
 echo luas_lingkaran($r);
+
+//Contoh 2
+function tambah_string ($str) {
+$str = $str . ", Jakarta";
+return $str;
+}
+//
+$str = "Universitas Budi Luhur";
+echo "\$str = $str<br>";
+echo tambah_string ($str). "<br>";
+echo "\$str = $str<br>";
 ?>
